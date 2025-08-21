3946792d4e84864e7beebd46a740c17aaa11c235
Bruno <brbaricitrin@gmail.com>
2025-08-20 23:13:20 -0300

    Inicio projeto biblioteca

---
+++ cadastrolivros/src/main/java/com/biblioteca/cadastrolivros/CadastrolivrosApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.cadastrolivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrolivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrolivrosApplication.class, args);
+	}
+
+}
---
+++ cadastrolivros/src/test/java/com/biblioteca/cadastrolivros/CadastrolivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.cadastrolivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrolivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
